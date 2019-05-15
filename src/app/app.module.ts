7d3ce57ba3c5e52122a6b98673ac2d323a4a5dd4
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2019-05-14 21:13:25 -0300

    Implementando tela de login e protegendo rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthService } from './services/login/auth.service';
+import { AuthGuard } from './guards/auth-guard';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guards/auth-guard';
+
+export const ROUTES: Routes = [
+     
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    }
+
+]
